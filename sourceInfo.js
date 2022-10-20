c955ff6312da6e57e7df38b2d62d103487c1dde5
Lenard Frommelt <lenny09918050@gmail.com>
2022-10-20 10:25:25 +0300

    1

--- sourceInfo.js
+++ sourceInfo.js
@@ -1,6 +1,10 @@
 module.exports = {
     getDependencies: () => {
         return {
+            "uuid": "^9.0.0",
+            "body-parser": "^1.20.1",
+            "express": "^4.18.2",
+            "express-ws": "^5.0.2"    
         }
     }
 }
